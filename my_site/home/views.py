d3a50545c5222c3cd505b5e99f1176accb6e7039
kilimovaann <annkilimova85@gmail.com>
2024-11-05 16:55:16 +0000

    gg

--- my_site/home/views.py
+++ my_site/home/views.py
@@ -4,43 +4,43 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def home_view(request):
-    return HttpResponse("Ласкаво просимо на головну сторінку")
-
-def about_view(request):
-    return HttpResponse("Про нас")
-
-def contact_view(request):
-    return HttpResponse("Зв'яжіться з нами")
-
-# Динамічні представлення
-def post_view(request, id):
-    return HttpResponse(f"Ви переглядаєте пост з ID: {id}")
-
-def profile_view(request, username):
-    return HttpResponse(f"Ви переглядаєте профіль користувача: {username}")
-
-def event_view(request, year, month, day):
-    return HttpResponse(f"Дата події: {year}-{month}-{day}")
-
-
-
 # def home_view(request):
-#     return render(request, 'home/home.html')
+#     return HttpResponse("Ласкаво просимо на головну сторінку")
 #
 # def about_view(request):
-#     return render(request, 'home/about.html')
+#     return HttpResponse("Про нас")
 #
 # def contact_view(request):
-#     return render(request, 'home/contact.html')
+#     return HttpResponse("Зв'яжіться з нами")
 #
+# # Динамічні представлення
 # def post_view(request, id):
-#     return render(request, 'home/post.html', {'id': id})
+#     return HttpResponse(f"Ви переглядаєте пост з ID: {id}")
 #
 # def profile_view(request, username):
-#     return render(request, 'home/profile.html', {'username': username})
+#     return HttpResponse(f"Ви переглядаєте профіль користувача: {username}")
 #
 # def event_view(request, year, month, day):
-#     return render(request, 'home/event.html', {'year': year, 'month': month, 'day': day})
+#     return HttpResponse(f"Дата події: {year}-{month}-{day}")
+
+
+
+def home_view(request):
+    return render(request, 'home/home.html')
+
+def about_view(request):
+    return render(request, 'home/about.html')
+
+def contact_view(request):
+    return render(request, 'home/contact.html')
+
+def post_view(request, id):
+    return render(request, 'home/post.html', {'id': id})
+
+def profile_view(request, username):
+    return render(request, 'home/profile.html', {'username': username})
+
+def event_view(request, year, month, day):
+    return render(request, 'home/event.html', {'year': year, 'month': month, 'day': day})
 
 
